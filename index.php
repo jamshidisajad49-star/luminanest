55406e416f023c8c9bb638790a6fe9b711782bed
jamshidisajad49-star <jamshidisajad49@gmail.com>
2026-03-05 16:34:33 +0530

    Update index.php

--- index.php
+++ index.php
@@ -896,6 +896,17 @@
             transform: translateY(0);
         }
     </style>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-YTP08B4056"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-YTP08B4056');
+</script>
+
+    
 </head>
 <body>
     <header class="site-header">
